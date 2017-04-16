eb2ea5cddd1ef5886b258a9a04a05b56ff2fbc0b
Filip Gulán <raipergm34@gmail.com>
2017-04-16 15:05:32 +0200

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#Git
+sudo apt-get update
+sudo apt-get install git
+
+#Node.js
+sudo apt-get update
+sudo apt-get install nodejs
+
+#NPM
+sudo apt-get update
+sudo apt-get install npm
+
+#PHP
+apt-get install libapache2-mod-php
+
+#Bower
+sudo npm install -g bower
+
+#Grunt
+sudo npm install -g grunt-cli
+
+#Cordova
+sudo npm install -g cordova
+
+#Composer
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+#JDK
+sudo apt-get update
+sudo apt-get install default-jdk
+
+#Ant
+sudo apt-get update
+sudo apt-get install ant
+
+#Crosswalk
+sudo npm install -g crosswalk-app-tools
+
+#FilleZilla
+sudo apt install filezilla
+
+#NW builder
+sudo npm install nw-builder -g
+
+#Angular CLI
+sudo npm install -g @angular/cli
+
+#Netbeans
+#Crosswalk
+#Nw.js
+
+#XAMP
+wget http://sourceforge.net/projects/xampp/files/XAMPP%20Linux/1.8.3/xampp-linux-x64-1.8.3-2-installer.run/download
